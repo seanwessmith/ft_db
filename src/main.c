82a097b91fd996b348322783a8a7537d6a12a927
Sean Smith <ssmith@student.42.us.org>
2017-04-20 10:45:22 -0700

    parsing inputs

--- src/main.c
+++ src/main.c
@@ -2,12 +2,62 @@
 
 #include "libft.h"
 
-int		main(int ac, char **av)
+int		q_type(char *q_type, char *input)
+{
+	while (*q_type)
+	{
+		if (*q_type != *input)
+			return (0);
+		input++;
+		q_type++;
+	}
+	return (1);
+}
+
+void	create_query(char *line)
+{
+	line = NULL;
+	write(0, "create\n", 7);
+}
+
+void	insert_query(char *line)
+{
+	line = NULL;
+	write(0, "insert\n", 7);
+
+}
+
+void	delete_query(char *line)
+{
+	line = NULL;
+	write(0, "delete\n", 8);
+
+}
+void	select_query(char *line)
+{
+	line = NULL;
+	write(0, "select\n", 7);
+
+}
+
+void	read_input()
 {
 	char	*line;
 
-	ac = 0;
-	av = NULL;
 	while (get_next_line(0, &line) == 1)
-		printf("line: %s", line);
+	{
+		if (q_type("create", line))
+			create_query(line);
+		else if (q_type("insert", line))
+			insert_query(line);
+		else if (q_type("delete", line))
+			delete_query(line);
+		else if (q_type("select", line))
+			select_query(line);
+	}
+}
+
+int		main()
+{
+	read_input();
 }
